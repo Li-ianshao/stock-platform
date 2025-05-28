1373150c291d9089b96135e42ba2a24baf2e1359
Shao Li Yan <poi7415778@gmail.com>
2025-05-27 21:48:41 -0500

    add venv與簡化url

--- stock_platform/urls.py
+++ stock_platform/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('tab_dividend')),
+    #path('', lambda request: redirect('tab_dividend')),
+    path('', lambda request: HttpResponse("OK")),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('stocks/', include('stocks.urls')),
